1806a0ebd6d026707c0b6f4ce6cfc81db4f95d91
Ajay22032001 <ajaypatwa2001@gmail.com>
2026-03-27 13:48:23 +0530

    Added PostgreSQL config for production

--- smart_assessment/settings.py
+++ smart_assessment/settings.py
@@ -164,9 +164,14 @@ from django.contrib.auth import get_user_model
 
 if os.getenv("CREATE_SUPERUSER") == "True":
     User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
+    
+    username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+    email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+    password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+    if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(
-            username="admin",
-            email="admin@gmail.com",
-            password="admin123"
+            username=username,
+            email=email,
+            password=password
         )
